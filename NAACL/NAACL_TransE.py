923b663d314c81b4aa0fe80f366ad0badfe9bb77
Sameer Singh <sameeersingh@gmail.com>
2015-02-22 16:48:58 -0800

    fix to get NAACL running

--- NAACL/NAACL_TransE.py
+++ NAACL/NAACL_TransE.py
@@ -3,5 +3,5 @@ from FB15k_exp import *
 
 launch(datapath='./data/',
     dataset='NAACL', op='TransE', simfn='L1', ndim=50, nhid=50, marge=1., lremb=0.01, lrparam=1.,
-    nbatches=1, totepochs=500, test_all=10, neval=1000, savepath='NAACL', Nent=24518, Nsyn=28629, Nrel=4111)
+    nbatches=100, totepochs=500, test_all=10, neval=1000, savepath='NAACL', Nent=28629, Nsyn=24518, Nrel=4111)
 
